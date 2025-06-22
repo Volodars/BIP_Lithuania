d1c38e62872ee44036673607a4195aeaa8e7d88f
Volodars <79505139+Volodars@users.noreply.github.com>
2025-06-23 02:09:58 +0300

    Add files via upload

---
+++ BIP-Project-Team-Yellow-main/BIP Projec/KVK_DataAccess/KVK_DataAccess/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace KVK_DataAccess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.WeatherDataTimer = new System.Windows.Forms.Timer(this.components);
+            this.BuyingPrice = new System.Windows.Forms.TextBox();
+            this.ComingHomeTime = new System.Windows.Forms.TextBox();
+            this.BuyingPriceLabel = new System.Windows.Forms.Label();
+            this.ComingHomeTimeLabel = new System.Windows.Forms.Label();
+            this.ISSLabel = new System.Windows.Forms.Label();
+            this.BatteryLabel = new System.Windows.Forms.Label();
+            this.CurrentPricesLabel = new System.Windows.Forms.Label();
+            this.BatteryStatusLabel = new System.Windows.Forms.Label();
+            this.ISSValue = new System.Windows.Forms.Label();
+            this.BatteryValue = new System.Windows.Forms.Label();
+            this.CurrentPricesValue = new System.Windows.Forms.Label();
+            this.BatteryStatusValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // WeatherDataTimer
+            // 
+            this.WeatherDataTimer.Enabled = true;
+            this.WeatherDataTimer.Interval = 4000;
+            this.WeatherDataTimer.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // BuyingPrice
+            // 
+            this.BuyingPrice.Location = new System.Drawing.Point(168, 53);
+            this.BuyingPrice.Name = "BuyingPrice";
+            this.BuyingPrice.Size = new System.Drawing.Size(100, 22);
+            this.BuyingPrice.TabIndex = 0;
+            this.BuyingPrice.Text = "0.0035";
+            // 
+            // ComingHomeTime
+            // 
+            this.ComingHomeTime.Location = new System.Drawing.Point(168, 116);
+            this.ComingHomeTime.Name = "ComingHomeTime";
+            this.ComingHomeTime.Size = new System.Drawing.Size(100, 22);
+            this.ComingHomeTime.TabIndex = 1;
+            this.ComingHomeTime.Text = "17";
+            // 
+            // BuyingPriceLabel
+            // 
+            this.BuyingPriceLabel.AutoSize = true;
+            this.BuyingPriceLabel.Location = new System.Drawing.Point(38, 56);
+            this.BuyingPriceLabel.Name = "BuyingPriceLabel";
+            this.BuyingPriceLabel.Size = new System.Drawing.Size(87, 16);
+            this.BuyingPriceLabel.TabIndex = 2;
+            this.BuyingPriceLabel.Text = "Buying price: ";
+            // 
+            // ComingHomeTimeLabel
+            // 
+            this.ComingHomeTimeLabel.AutoSize = true;
+            this.ComingHomeTimeLabel.Location = new System.Drawing.Point(38, 116);
+            this.ComingHomeTimeLabel.Name = "ComingHomeTimeLabel";
+            this.ComingHomeTimeLabel.Size = new System.Drawing.Size(124, 16);
+            this.ComingHomeTimeLabel.TabIndex = 3;
+            this.ComingHomeTimeLabel.Text = "Coming home time: ";
+            // 
+            // ISSLabel
+            // 
+            this.ISSLabel.AutoSize = true;
+            this.ISSLabel.Location = new System.Drawing.Point(38, 190);
+            this.ISSLabel.Name = "ISSLabel";
+            this.ISSLabel.Size = new System.Drawing.Size(34, 16);
+            this.ISSLabel.TabIndex = 4;
+            this.ISSLabel.Text = "ISS: ";
+            // 
+            // BatteryLabel
+            // 
+            this.BatteryLabel.AutoSize = true;
+            this.BatteryLabel.Location = new System.Drawing.Point(38, 215);
+            this.BatteryLabel.Name = "BatteryLabel";
+            this.BatteryLabel.Size = new System.Drawing.Size(55, 16);
+            this.BatteryLabel.TabIndex = 5;
+            this.BatteryLabel.Text = "Battery: ";
+            // 
+            // CurrentPricesLabel
+            // 
+            this.CurrentPricesLabel.AutoSize = true;
+            this.CurrentPricesLabel.Location = new System.Drawing.Point(38, 240);
+            this.CurrentPricesLabel.Name = "CurrentPricesLabel";
+            this.CurrentPricesLabel.Size = new System.Drawing.Size(95, 16);
+            this.CurrentPricesLabel.TabIndex = 6;
+            this.CurrentPricesLabel.Text = "Current prices: ";
+            // 
+            // BatteryStatusLabel
+            // 
+            this.BatteryStatusLabel.AutoSize = true;
+            this.BatteryStatusLabel.Location = new System.Drawing.Point(38, 269);
+            this.BatteryStatusLabel.Name = "BatteryStatusLabel";
+            this.BatteryStatusLabel.Size = new System.Drawing.Size(93, 16);
+            this.BatteryStatusLabel.TabIndex = 7;
+            this.BatteryStatusLabel.Text = "Battery status: ";
+            // 
+            // ISSValue
+            // 
+            this.ISSValue.AutoSize = true;
+            this.ISSValue.Location = new System.Drawing.Point(78, 190);
+            this.ISSValue.Name = "ISSValue";
+            this.ISSValue.Size = new System.Drawing.Size(14, 16);
+            this.ISSValue.TabIndex = 8;
+            this.ISSValue.Text = "0";
+            // 
+            // BatteryValue
+            // 
+            this.BatteryValue.AutoSize = true;
+            this.BatteryValue.Location = new System.Drawing.Point(131, 215);
+            this.BatteryValue.Name = "BatteryValue";
+            this.BatteryValue.Size = new System.Drawing.Size(14, 16);
+            this.BatteryValue.TabIndex = 9;
+            this.BatteryValue.Text = "0";
+            // 
+            // CurrentPricesValue
+            // 
+            this.CurrentPricesValue.AutoSize = true;
+            this.CurrentPricesValue.Location = new System.Drawing.Point(148, 240);
+            this.CurrentPricesValue.Name = "CurrentPricesValue";
+            this.CurrentPricesValue.Size = new System.Drawing.Size(14, 16);
+            this.CurrentPricesValue.TabIndex = 10;
+            this.CurrentPricesValue.Text = "0";
+            // 
+            // BatteryStatusValue
+            // 
+            this.BatteryStatusValue.AutoSize = true;
+            this.BatteryStatusValue.Location = new System.Drawing.Point(148, 269);
+            this.BatteryStatusValue.Name = "BatteryStatusValue";
+            this.BatteryStatusValue.Size = new System.Drawing.Size(14, 16);
+            this.BatteryStatusValue.TabIndex = 11;
+            this.BatteryStatusValue.Text = "0";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(574, 392);
+            this.Controls.Add(this.BatteryStatusValue);
+            this.Controls.Add(this.CurrentPricesValue);
+            this.Controls.Add(this.BatteryValue);
+            this.Controls.Add(this.ISSValue);
+            this.Controls.Add(this.BatteryStatusLabel);
+            this.Controls.Add(this.CurrentPricesLabel);
+            this.Controls.Add(this.BatteryLabel);
+            this.Controls.Add(this.ISSLabel);
+            this.Controls.Add(this.ComingHomeTimeLabel);
+            this.Controls.Add(this.BuyingPriceLabel);
+            this.Controls.Add(this.ComingHomeTime);
+            this.Controls.Add(this.BuyingPrice);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetInfo;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer WeatherDataTimer;
+        private System.Windows.Forms.TextBox BuyingPrice;
+        private System.Windows.Forms.TextBox ComingHomeTime;
+        private System.Windows.Forms.Label BuyingPriceLabel;
+        private System.Windows.Forms.Label ComingHomeTimeLabel;
+        private System.Windows.Forms.Label ISSLabel;
+        private System.Windows.Forms.Label BatteryLabel;
+        private System.Windows.Forms.Label CurrentPricesLabel;
+        private System.Windows.Forms.Label BatteryStatusLabel;
+        private System.Windows.Forms.Label ISSValue;
+        private System.Windows.Forms.Label BatteryValue;
+        private System.Windows.Forms.Label CurrentPricesValue;
+        private System.Windows.Forms.Label BatteryStatusValue;
+    }
+}
+
---
+++ BIP-Project-Team-Yellow-main/BIP Projec/KVK_DataAccess/KVK_DataAccess/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Windows.Forms;
+
+using KVK_DataAccess.EWS_PME;
+
+using Newtonsoft.Json.Linq;
+
+
+namespace KVK_DataAccess
+{
+    enum BatteryStatus
+    {
+        Inactive, Charging, Discharging
+    }
+
+    public partial class Form1 : Form
+    {
+        double CurrentPrices = 0.1;
+        List<string> Forecast = new List<string>();
+        List<int> m_Codes = new List<int>();
+
+        BatteryStatus CurrentBatteryStatus = 0;
+        double BUYING_PRICE = 0;
+        int COMING_HOME_TIME = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            COMING_HOME_TIME = Convert.ToInt32(ComingHomeTime.Text);
+            BUYING_PRICE = Convert.ToDouble(BuyingPrice.Text);
+            mainLoop();
+        }
+
+        async void timer1_Tick(object sender, EventArgs e)
+        {
+            mainLoop();
+          
+            CurrentPricesValue.Text = $"{CurrentPrices} EUR/kWh";
+            BatteryStatusValue.Text = $"{CurrentBatteryStatus.ToString()}";
+        }
+
+        void mainLoop()
+        {
+            List<double> list = GetPower();
+            NordPool();
+            double battery = list[1];
+            double ISS = list[0];
+
+            ISSValue.Text = $"{ISS}";
+            BatteryValue.Text = $"{battery}";
+
+            int currentTime = DateTime.Now.Hour;
+
+            if (battery < 15)
+            {
+                Console.WriteLine("charging");
+                MSSQL(BatteryStatus.Charging, 5);
+                return;
+            }
+
+            // Night time conditions
+            bool isNight = currentTime < 6 || currentTime >= 23;
+
+            if (isNight)
+            {
+                Console.WriteLine(m_Codes[0] < 3 ? "sunny and discharging" : "bad weather and inactive");
+                MSSQL(m_Codes[0] < 3 ? BatteryStatus.Discharging : BatteryStatus.Inactive, 5);
+                return;
+            }
+
+            // Approaching home time
+            if (currentTime > COMING_HOME_TIME - 2 && currentTime < COMING_HOME_TIME)
+            {
+                Console.WriteLine(battery < 99 ? "we are charging" : "inactive");
+                MSSQL(battery < 99 ? BatteryStatus.Charging : BatteryStatus.Inactive, 5);
+                return;
+            }
+
+            // ISS is active
+            if (ISS > 0)
+            {
+                if (CurrentPrices < BUYING_PRICE)
+                {
+                    Console.WriteLine(battery < 55
+                        ? "not enough electricity and low price - buying, we are charging"
+                        : "too expensive we are inactive");
+                    MSSQL(battery < 55 ? BatteryStatus.Charging : BatteryStatus.Inactive, 5);
+                }
+                else
+                {
+                    Console.WriteLine("discharging");
+                    MSSQL(BatteryStatus.Discharging, 5);
+                }
+                return;
+            }
+
+            // ISS is not active
+            bool isAfternoon = currentTime > 12 && currentTime < 18;
+            Console.WriteLine((isAfternoon && battery < 99) ? "charging" : "inactive");
+            MSSQL((isAfternoon && battery < 99) ? BatteryStatus.Charging : BatteryStatus.Inactive, 5);
+        }
+
+
+
+        async void Weather(int daysToForecast)
+        {
+            string url = "https://api.open-meteo.com/v1/forecast?latitude=55.7033&longitude=21.1443&daily=temperature_2m_max,temperature_2m_min,weathercode&timezone=Europe%2FRiga";
+
+            HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync(url);
+
+            JsonDocument doc = JsonDocument.Parse(response);
+            var root = doc.RootElement;
+
+            var dates = root.GetProperty("daily").GetProperty("time");
+            var codes = root.GetProperty("daily").GetProperty("weathercode");
+
+            Dictionary<int, string> weatherCodeMap = new Dictionary<int, string>
+            {
+                {0, "Clear sky"},
+                {1, "Mainly clear"},
+                {2, "Partly cloudy"},
+                {3, "Cloudy"},
+                {45, "Fog"}, {48, "Depositing rime fog"},
+                {51, "Drizzle (light)"}, {53, "Drizzle (moderate)"}, {55, "Drizzle (heavy)"},
+                {56, "Freezing drizzle (light)"}, {57, "Freezing drizzle (heavy)"},
+                {61, "Rain (light)"}, {63, "Rain (moderate)"}, {65, "Rain (heavy)"},
+                {66, "Freezing rain (light)"}, {67, "Freezing rain (heavy)"},
+                {71, "Snowfall (light)"}, {73, "Snowfall (moderate)"}, {75, "Snowfall (heavy)"},
+                {77, "Snow grains"}, {80, "Rain showers (light)"}, {81, "Rain showers (moderate)"}, {82, "Rain showers (heavy)"},
+                {85, "Snow showers (light)"}, {86, "Snow showers (heavy)"},
+                {95, "Thunderstorm (light)"}, {96, "Thunderstorm (moderate)"}, {99, "Thunderstorm (heavy)"}
+            };
+
+            StringBuilder weatherInfo = new StringBuilder();
+
+            m_Codes.Clear();
+            for (int i = 0; i < daysToForecast; i++)
+            {
+                string date = dates[i].GetString();
+                int code = codes[i].GetInt32();
+                m_Codes.Add(codes[i].GetInt32());
+                string description = weatherCodeMap.ContainsKey(code) ? weatherCodeMap[code] : "Unknown";
+                weatherInfo.AppendLine($"[{date}] Weather: {description}");
+            }
+        }
+
+        async void NordPool()
+        {
+            var client = new HttpClient();
+            string url = "https://dashboard.elering.ee/api/nps/price";
+
+            var response = await client.GetStringAsync(url);
+            JObject json = JObject.Parse(response);
+
+            var prices = json["data"]["ee"];
+
+            foreach (var item in prices)
+            {
+                long timestamp = item["timestamp"].Value<long>();
+                CurrentPrices = item["price"].Value<double>() / 1000;
+            }
+        }
+
+        List<double> GetPower()
+        {
+            var client = new DataExchangeClient();
+            client.ClientCredentials.HttpDigest.ClientCredential = new NetworkCredential("kvk", "KvK-DataAccess1", "172.16.16.60");
+
+            client.ClientCredentials.HttpDigest.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
+            try
+            {
+                // Create the request object
+                var request = new GetValuesRequest
+                {
+                    GetValuesIds = new[] { "4@1042@V", "9@-685@V" }, // Replace with actual value ID
+                    version = "1.0"
+                };
+
+                // Call the service method
+                var response = client.GetValues(request);
+
+                if (response.GetValuesItems == null || response.GetValuesItems.Length == 0)
+                {
+                    return new List<double>();
+                }
+
+                var ISS = response.GetValuesItems[0];
+                var battery = response.GetValuesItems[1];
+
+                List<double> list = new List<double>();
+                list.Add(Convert.ToDouble(ISS.Value));
+                list.Add(Convert.ToDouble(battery.Value));
+                return list;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Service Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                client.Close();
+            }
+
+            return new List<double>();
+        }
+
+        void MSSQL(BatteryStatus batteryStatus, int ElectricityScenario)
+        {
+            CurrentBatteryStatus = batteryStatus;
+            string connectionString = "Server=172.16.16.60;Database=BIP_Project;User Id=kvk;Password=KvK-DataAccess1;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Corrected query with parameters
+                string updateQuery = "UPDATE BIP_Result SET BatteryStatus = @batteryStatus, Scene = @Scenario WHERE Team_Name = 'Yellow_Team'";
+
+                using (SqlCommand command = new SqlCommand(updateQuery, connection))
+                {
+                    // Add parameters with proper values
+                    command.Parameters.AddWithValue("@batteryStatus", Convert.ToInt32(batteryStatus));
+                    command.Parameters.AddWithValue("@Scenario", ElectricityScenario);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Weather(3);
+        }
+    }
+}
